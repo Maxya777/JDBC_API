3355799e6e187ef466d3c0f91eb420adeac7511b
Maxya777 <max3532@yandex.ru>
2022-12-15 14:27:34 +0300

    added new functionality 2

--- src/main/java/com/example/jdbc_api/SecurityConfiguration.java
+++ src/main/java/com/example/jdbc_api/SecurityConfiguration.java
@@ -2,43 +2,56 @@ package com.example.jdbc_api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
+
+    @Bean
+    PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                .antMatchers("/persons/by-age").permitAll()
-                .antMatchers("/persons/by-city").hasRole("USER")
-                .antMatchers("/persons/get-data").hasRole("ADMIN")
-                .and()
-                .formLogin();
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails visitor = User.withDefaultPasswordEncoder()
+                .username("visitor")
+                .password("qwerty")
+                .roles("READ")
+                .build();
+
         UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+                .username("Maxim")
                 .password("password")
-                .roles("USER")
+                .roles("READ", "WRITE")
                 .build();
 
         UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+                .username("Pavel")
                 .password("password")
-                .roles("ADMIN", "USER")
+                .roles("READ", "WRITE", "DELETE")
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        return new InMemoryUserDetailsManager(user, admin, visitor);
     }
 }
 
--- src/main/java/com/example/jdbc_api/controller/JdbcController.java
+++ src/main/java/com/example/jdbc_api/controller/JdbcController.java
@@ -2,10 +2,13 @@ package com.example.jdbc_api.controller;
 
 import com.example.jdbc_api.model.Person;
 import com.example.jdbc_api.repository.RepositoryJdbc;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,17 +21,20 @@ public class JdbcController {
         this.repositoryJdbc = repositoryJdbc;
     }
 
+    @PreAuthorize("hasAnyRole('WRITE', 'DELETE') && #name == authentication.principal.username")
     @GetMapping("/persons/get-data")
     public Optional<Person> findByNameAndSurname(@RequestParam("name") String name,
                                                  @RequestParam("surname") String surname) {
         return repositoryJdbc.findByNameAndSurname(name, surname);
     }
 
+    @RolesAllowed("ROLE_WRITE")
     @GetMapping("/persons/by-city")
     public List<Person> findByCity(@RequestParam("city") String city) {
         return repositoryJdbc.findByCity(city);
     }
 
+    @Secured("ROLE_READ")
     @GetMapping("/persons/by-age")
     public List<Person> findByAgeLessThanOrderByAge(@RequestParam("age") int age) {
         return repositoryJdbc.findByAgeLessThanOrderByAge(age);
